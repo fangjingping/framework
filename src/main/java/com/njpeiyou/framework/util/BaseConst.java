8cdc723057363891f56ca687328ed3d6fe62ad97
方京平 <fangjingping-xes@fangjingpingdeMac-mini.local>
2017-04-28 14:22:54 +0800

    change class to interface

--- src/main/java/com/njpeiyou/framework/util/BaseConst.java
+++ src/main/java/com/njpeiyou/framework/util/BaseConst.java
@@ -3,12 +3,12 @@ package com.njpeiyou.framework.util;
 /**
  * Created by fangjingping-xes on 2017/4/1.
  */
-public class BaseConst {
+public interface BaseConst {
 
-    public static final int CODE_SUCCESS = 0;
-    public static final int CODE_FAIL = 1;
-    public static final int CODE_ERROR = 2;
-    public static final String MSG_FAIL = "logic error";
+    int CODE_SUCCESS = 0;
+    int CODE_FAIL = 1;
+    int CODE_ERROR = 2;
+    String MSG_FAIL = "logic error";
 
 
 }
