00bd284580ebf2cf1e639b3fa35f8f64badcb320
方京平 <fangjingping-xes@fangjingpingdeMac-mini.local>
2017-05-10 09:26:54 +0800

    add REUtil.java

---
+++ src/main/java/com/njpeiyou/framework/util/REUtil.java
@@ -0,0 +1,29 @@
+package com.njpeiyou.framework.util;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by fangjingping-xes on 2017/4/1.
+ */
+public class REUtil {
+
+    public static Boolean isUrl(String url){
+        if(StringUtils.isEmpty(url)){return false;}
+        String regEx="^((https|http|ftp|rtsp|mms)?:\\/\\/)[^\\s]+";
+        Pattern pattern = Pattern.compile(regEx);
+        Matcher matcher = pattern.matcher(url);
+        boolean match = matcher.matches();
+        return match;
+    }
+
+    public static Boolean isEmail(String email){
+        if(StringUtils.isEmpty(email)){return false;}
+        String regEx="^[A-Za-zd]+([-_.][A-Za-zd]+)*@([A-Za-zd]+[-.])+[A-Za-zd]{2,5}$";
+        Pattern pattern = Pattern.compile(regEx);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+}
