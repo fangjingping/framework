79613b688d10246c9e0fc74b3061a9c039d9611a
fangjingping <fangjingping@aliyun.com>
2018-01-30 14:51:45 +0800

    update

--- src/main/java/com/njpeiyou/framework/interceptor/CrossOriginInterceptor.java
+++ src/main/java/com/njpeiyou/framework/interceptor/CrossOriginInterceptor.java
@@ -12,7 +12,8 @@ public class CrossOriginInterceptor implements Interceptor{
         inv.getController().getResponse().setHeader("Access-Control -Allow-Origin","*");
         inv.getController().getResponse().setHeader("Access-Control-Allow-Methods", "GET,PUT,DELETE,OPTIONS");
 //        inv.getController().getResponse().setHeader("Access-Control-Allow-Headers", "Content-Type");
-        inv.getController().getResponse().setHeader("Access-Control-Allow-Headers", "Accept,Origin,XRequestedWith,Content-Type,LastModified,content-type");
+//        inv.getController().getResponse().setHeader("Access-Control-Allow-Headers", "Accept,Origin,XRequestedWith,Content-Type,LastModified,content-type");
+        inv.getController().getResponse().setHeader("Access-Control-Allow-Headers", "content-type");
         inv.invoke();
     }
 }
