bc2787606ed0fd4955c1b13d19383e2e1f351376
fangjingping <fangjingping@aliyun.com>
2018-01-30 10:18:42 +0800

    继续增加跨域设置。设置了Origin Methods Headers

--- src/main/java/com/njpeiyou/framework/interceptor/CrossOriginInterceptor.java
+++ src/main/java/com/njpeiyou/framework/interceptor/CrossOriginInterceptor.java
@@ -11,6 +11,8 @@ public class CrossOriginInterceptor implements Interceptor{
     public void intercept(Invocation inv) {
         inv.getController().getResponse().setHeader("Access-Control-Allow-Origin","*");
         inv.getController().getResponse().setHeader("Access-Control-Allow-Methods", "GET,PUT,DELETE,OPTIONS");
+//        inv.getController().getResponse().setHeader("Access-Control-Allow-Headers", "Content-Type");
+        inv.getController().getResponse().setHeader("Access-Control-Allow-Headers", "Accept,Origin,XRequestedWith,Content-Type,LastModified");
         inv.invoke();
     }
 }
